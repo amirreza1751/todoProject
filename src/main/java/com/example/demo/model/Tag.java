a201daf311b48a932f613360f38ae3c0199269be
amirreza1751 <dashti.amir2752@gmail.com>
2023-02-27 16:14:45 +0100

    models and repositories created for users and tag

--- src/main/java/com/example/demo/model/Tag.java
+++ src/main/java/com/example/demo/model/Tag.java
@@ -1,9 +1,13 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
 import java.sql.Timestamp;
 import java.util.Set;
 
@@ -12,33 +16,19 @@ import java.util.Set;
 @AllArgsConstructor
 @Data
 @Builder
-public class Todo {
+public class Tag {
     @Id
     @GeneratedValue
     @Column(updatable = false, nullable = false)
     Long id;
     @Column
-    String title;
-    @Column
-    String description;
-    @Column
-    TodoStatus todoStatus;
+    String name;
     @CreationTimestamp
     @Column(updatable = false)
     Timestamp dateCreated;
     @UpdateTimestamp
     Timestamp lastModified;
 
-    @ManyToMany
-    @JoinTable(
-            name = "todo_tag",
-            joinColumns = @JoinColumn(name = "todo_id"),
-            inverseJoinColumns = @JoinColumn(name = "tag_id")
-    )
-    Set<Tag> tagSet;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private Users user;
-
+    @ManyToMany(mappedBy = "tagSet")
+    Set<Todo> todoSet;
 }
