a66751005f37046ac65bf97f2d6fbbf5f68d758d
amirreza1751 <dashti.amir2752@gmail.com>
2023-03-01 17:23:47 +0100

    user mapper small update

--- src/main/java/com/example/demo/mapper/user/UsersMapper.java
+++ src/main/java/com/example/demo/mapper/user/UsersMapper.java
@@ -1,8 +1,12 @@
 package com.example.demo.mapper.user;
 
+import com.example.demo.DTO.todo.TodoDTO;
 import com.example.demo.DTO.user.UsersDTO;
+import com.example.demo.model.Todo;
 import com.example.demo.model.Users;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import java.util.Collection;
 import java.util.List;
@@ -12,4 +16,10 @@ public abstract class UsersMapper {
     public abstract Users toEntity(UsersDTO usersDTO);
     public abstract UsersDTO toDto(Users user);
     public abstract List<UsersDTO> toDTO (Collection<Users> users);
+    @AfterMapping
+    protected void addOwnerNameToTodoDTO(Users user, @MappingTarget UsersDTO usersDTO){
+        if (usersDTO.getTodoSet().size() == 0)
+            return;
+        usersDTO.getTodoSet().forEach(todoDTO -> todoDTO.setOwner(user.getName()));
+    }
 }
